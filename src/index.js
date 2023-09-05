fec49b66aeedb6882d8d14c3c86380805dba6af6
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-05 10:49:26 -0700

    add .env to gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+// Business Logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5ad4f76e2341415f820db91975a3388e`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+// UI Logic
+
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
